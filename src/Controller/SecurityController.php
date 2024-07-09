11fe665a3676862bb42ab51afada66eccc5b588e
Nomy <noemielimbasse@gmail.com>
2024-07-09 17:34:31 +0200

    Documentation de l'API

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\{JsonResponse, Request, Response};
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -11,15 +12,46 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Serializer\SerializerInterface;
 
+use function PHPSTORM_META\type;
+
 #[Route('/api', name: 'app_api_')]
 class SecurityController extends AbstractController
 {
     public function __construct(private SerializerInterface $serializer, private EntityManagerInterface $manager)
     {
-
     }
-
+    
     #[Route('/registration', name: 'registration', methods: 'POST')]
+    #[OA\Post(
+        path: "/api/registration",
+        summary: "Inscription d'un nouvel utilisateur",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "Données de l'utilisateur à inscrire",
+            content: new OA\JsonContent(
+                type: "object",
+                properties: [
+                    new OA\Property(property: "email", type: "string", example: "adresse@mail.com"),
+                    new OA\Property(property: "password", type: "string", example: "Mot de passe"),
+                    new OA\Property(property: "roles", type: "array", items: new OA\Items(type: "string", example: "ROLE_USER"))
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: "Utilisateur inscrit avec succès",
+                content: new OA\JsonContent(
+                    type: "object",
+                    properties: [
+                        new OA\Property(property: "user", type: "string", example: "adresse@mail.com"),
+                        new OA\Property(property: "apiToken", type: "string", example: "355s48f43sf628sef355s48f43sf628sef"),
+                        new OA\Property(property: "roles", type: "array", items: new OA\Items(type: "string", example: "ROLE_USER"))
+                    ]
+                )
+            )
+        ]
+    )]
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
         $user = $this->serializer->deserialize($request->getContent(), User::class,'json');
@@ -35,6 +67,34 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/login', name: 'login', methods: 'POST')]
+    #[OA\Post(
+        path:"/api/login",
+        summary: "Connecter un utilisateur",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "Données de l'utilisateur pour se connecter",
+            content : new OA\JsonContent(
+                type : "object",
+                properties: [
+                    new OA\Property(property: "username", type: "string", example:"adresse@mail.com"),
+                    new OA\Property(property:"password", type: "string", example: "Mot de passe")
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(
+            response: 200,
+            description: "Connexion réussie",
+            content: new OA\JsonContent(
+                type : "object",
+                properties: [
+                    new OA\Property(property: "user", type: "string", example:"Nom d'utilisateur"),
+                    new OA\Property(property:"apiToken", type: "string", example: "355s48f43sf628sef355s48f43sf628sef"),
+                    new OA\Property(property: "roles", type: "array", items: new OA\Items(type: "string", example: "ROLE_USER")),
+                ]
+            )
+        )]
+    )]
     public function login(#[CurrentUser] ?User $user): JsonResponse
     {
         if(null === $user){
--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -3,13 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Services;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{JsonResponse, Response, Request};
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ServicesRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -18,7 +17,7 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ServicesController extends AbstractController
 {
     public function __construct(
-        private EntityManagerInterface $manager, 
+        private EntityManagerInterface $manager,
         private ServicesRepository $repository,
         private SerializerInterface $serializer,
         private UrlGeneratorInterface $urlGenerator,
@@ -27,9 +26,37 @@ class ServicesController extends AbstractController
         
     }
     #[Route(name: 'new', methods: 'POST')]
+    #[OA\Post(
+        path: "/api/services",
+        summary: "Créer un service",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "Données du service à créer",
+            content: new OA\JsonContent(
+                type: "object",
+                properties: [
+                    "nom" => new OA\Property(property: "nom", type: "string", example: "Nom du service"),
+                    "description" =>new OA\Property(property: "description", type: "string", example: "Description du service")
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: "Service créé avec succès",
+                content: new OA\JsonContent(
+                    type: "object",
+                    properties: [
+                        "id" => new OA\Property(property: "id", type: "integer", example: "1"),
+                        "nom" => new OA\Property(property: "nom", type: "string", example: "Nom du service"),
+                        "description" => new OA\Property(property: "description", type: "string", example: "Description du service")
+                    ]
+                )
+            )
+        ]
+    )]
     public function new(Request $request): Response
     {
-        //Crée un nouvel utilisateur à l'aide d'un formulaire
         $services = $this->serializer->deserialize($request->getContent(), Services::class, 'json');
 
         $this->manager->persist($services);
@@ -46,6 +73,37 @@ class ServicesController extends AbstractController
     }
 
     #[Route('/{id}', name: 'show', methods: 'GET')]
+    #[OA\Get(
+        path: "/api/services/{id}",
+        summary: "Afficher un service par son ID",
+        parameters:[
+            new OA\Parameter(
+                name: "id",
+                in: "path",
+                required: "true",
+                description: "ID du service à afficher",
+                schema: new OA\Schema(type: "integer")
+            )
+        ],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Service trouvé avec succès",
+                content: new OA\JsonContent(
+                    type: "object",
+                    properties: [
+                        new OA\Property(property: "id", type: "integer", example:"1"),
+                        new OA\Property(property: "nom", type: "string", example:"Nom du service"),
+                        new OA\Property(property: "description", type: "string", example:"Description du service")
+                    ]
+                )
+            ),
+            new OA\Response(
+                response: 404,
+                description:"Service non trouvé"
+            )
+        ]
+    )]
     public function show(int $id): Response
     {
         $services = $this->repository->findOneBy(['id' => $id]);
@@ -59,6 +117,48 @@ class ServicesController extends AbstractController
     }
 
     #[Route('/{id}', name: 'edit', methods: 'PUT')]
+    #[OA\Put(
+        path: "/api/services/{id}",
+        summary: "Editer un service",
+        parameters:[
+            new OA\Parameter(
+                name: "id",
+                in: "path",
+                required: "true",
+                description: "ID du service à modifier",
+                schema: new OA\Schema(type: "integer")
+            )
+        ],
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "Données du service à modifier",
+            content: new OA\JsonContent(
+                type: "object",
+                properties: [
+                    new OA\Property(property: "nom", type: "string", example: "Nom du service"),
+                    new OA\Property(property: "description", type: "string", example: "Description du service")
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 204,
+                description: "Service modifié avec succès",
+                content: new OA\JsonContent(
+                    type: "object",
+                    properties: [
+                        new OA\Property(property: "id", type: "integer", example: "1"),
+                        new OA\Property(property: "nom", type: "string", example: "Nom du service"),
+                        new OA\Property(property: "description", type: "string", example: "Description du service")
+                    ]
+                )
+            ),
+            new OA\Response(
+                response: 404,
+                description:"Service non trouvé"
+            )
+        ]
+    )]
     public function edit(int $id, Request $request): Response
     {
         $services = $this->repository->findOneBy(['id' => $id]);
@@ -79,6 +179,29 @@ class ServicesController extends AbstractController
     }
 
     #[Route('/{id}', name: 'delete', methods: 'DELETE')]
+    #[OA\Delete(
+        path: "/api/services/{id}",
+        summary: "Supprimer un service par son ID",
+        parameters:[
+            new OA\Parameter(
+                name: "id",
+                in: "path",
+                required: "true",
+                description: "ID du service à supprimer",
+                schema: new OA\Schema(type: "integer")
+            )
+        ],
+        responses: [
+            new OA\Response(
+                response: 204,
+                description: "Service suprimé avec succès",
+            ),
+            new OA\Response(
+                response: 404,
+                description:"Service non trouvé"
+            )
+        ]
+    )]
     public function delete(int $id): Response
     {
         $services = $this->repository->findOneBy(['id' => $id]);
